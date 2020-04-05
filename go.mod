f2d3dae2c07bbf9cf6a9980e92a0caee785cde4a
Cody Seavey <seavey.cody@gmail.com>
2020-04-05 14:19:01 -0500

    Add stuff to assets and improve shutdown logic

--- go.mod
+++ go.mod
@@ -3,7 +3,26 @@ module github.com/codyseavey/test-app
 go 1.13
 
 require (
+	github.com/coreos/go-etcd v2.0.0+incompatible // indirect
+	github.com/cosiner/argv v0.1.0 // indirect
+	github.com/cpuguy83/go-md2man v1.0.10 // indirect
+	github.com/go-delve/delve v1.4.0 // indirect
+	github.com/gorilla/mux v1.7.3
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/mattn/go-colorable v0.1.6 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/peterh/liner v1.2.0 // indirect
 	github.com/shurcooL/httpfs v0.0.0-20190707220628-8d4bc4ba7749 // indirect
 	github.com/shurcooL/vfsgen v0.0.0-20181202132449-6a9ea43bcacd
+	github.com/sirupsen/logrus v1.5.0 // indirect
+	github.com/spf13/cobra v0.0.7 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/ugorji/go/codec v0.0.0-20181204163529-d75b2dcb6bc8 // indirect
+	go.starlark.net v0.0.0-20200330013621-be5394c419b6 // indirect
+	golang.org/x/arch v0.0.0-20200312215426-ff8b605520f4 // indirect
+	golang.org/x/crypto v0.0.0-20200117160349-530e935923ad // indirect
+	golang.org/x/sys v0.0.0-20200331124033-c3d80250170d // indirect
 	golang.org/x/tools v0.0.0-20191212224101-0f69de236bb7 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- main.go
+++ main.go
@@ -1,31 +1,97 @@
 package main
 
 import (
+	"context"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
 )
 
 //go:generate go run -mod=vendor vfs.go
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	file, err := assets.Open("/index.html")
+	vars := make(map[string]string)
+	vars["file"] = strings.TrimPrefix(r.URL.Path, "/")
+
+	log.Println(vars["file"])
+
+	file, err := assets.Open(vars["file"])
 	if err != nil {
 		w.WriteHeader(500)
-		w.Write([]byte(`Error getting index.html`))
+		w.Write([]byte(`Error getting ` + vars["file"]))
 	}
 	defer file.Close()
 
 	data, err := ioutil.ReadAll(file)
 	if err != nil {
 		w.WriteHeader(500)
-		w.Write([]byte(`Could not read index.html`))
+		w.Write([]byte(`Could not read ` + vars["file"]))
 	}
-	w.Write(data)
+
+	t := template.New("html")
+	t, err = t.Parse(string(data))
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte(`Could not read ` + vars["file"]))
+	}
+	t.Execute(w, t)
 }
 
 func main() {
-	http.HandleFunc("/", indexHandler)
-	log.Println("starting app...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	mux := http.NewServeMux()
+	server := http.Server{Addr: ":8080", Handler: mux}
+
+	var useTLS bool
+	var tlsServer *http.Server
+	tlsCert, tlsKey := os.Getenv("TLS_CERT"), os.Getenv("TLS_KEY")
+	if tlsCert != "" && tlsKey != "" {
+		useTLS = true
+		tlsServer = &http.Server{Addr: ":8443", Handler: mux}
+	} else {
+		useTLS = false
+		tlsServer = nil
+	}
+
+	mux.HandleFunc("/", indexHandler)
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	if useTLS {
+		go func() {
+			log.Print("https server started on port 8443")
+			if err := tlsServer.ListenAndServeTLS(tlsCert, tlsKey); err != nil && err != http.ErrServerClosed {
+				log.Fatalf("https listen failed: %s\n", err)
+			}
+		}()
+	}
+
+	go func() {
+		log.Print("http server started on port 8080")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("http listen failed: %s\n", err)
+		}
+	}()
+
+	<-done
+
+	// Attempt to gracefully stop the server on interupt
+	log.Print("server stopping...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		cancel()
+	}()
+
+	// Attempt to gracefully shutdown the server with a timeout
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("server shutdown failed: %v", err)
+	}
+
+	log.Print("server shutdown complete")
 }
